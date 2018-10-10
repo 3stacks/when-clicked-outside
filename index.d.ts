e7cd7f1c2e3d17177a353deb409329b079a2e8bd
Luke Boyle <luke.boyle.au@gmail.com>
2018-10-10 19:30:35 +1100

    chore: export granular types

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,8 @@
-declare function whenClickedOutside(element : HTMLElement, callback : (event : MouseEvent) => any) : {
+export interface ClickedOutsideListener {
     element : HTMLElement,
     destroy : () => void
-};
+}
+
+declare function whenClickedOutside(element : HTMLElement, callback : (event : MouseEvent) => any) : ClickedOutsideListener;
 
 export default whenClickedOutside;
